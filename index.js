d92e75c0d491ec685588ec7ab04b0de61e78b2c5
KYinXu <kyleyinxu@gmail.com>
2024-01-16 16:15:39 -0800

    add fade

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ window.onscroll = onScroll;
 {onScroll()};
 function onScroll(){
     nav = document.getElementById("navigation-bar");
-    console.log(nav.style.backgroundColor);
     if (document.body.scrollTop >= 15){
-        document.getElementById("navigation-bar").style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+        document.getElementById("navigation-bar").style.backgroundColor = "rgba(0, 0, 0, 1)";
+        bgFadeIn(document.getElementById("navigation-bar"));
     } else {
         document.getElementById("navigation-bar").style.backgroundColor = "transparent";
     }
     
+}
+
+function bgFadeIn(element) {
+    var op = 0.1;
+    var timer = setInterval(function(){
+        if (op >= 1){
+            clearInterval(timer);
+        }
+
+        //Fix here
+        element.style.opacity = op;
+        element.style.filter = "alpha(opacity=" + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+function bgFadeOut(element) {
+    var op = 1;
+    var timer = setInterval(function(){
+        if (op <= 0.0){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = "alpha(opacity=" + op * 100 + ")";
+        op -= op * 0.1;
+    }, 10);
 }
