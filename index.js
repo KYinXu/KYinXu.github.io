401c92b307d6c67b709cdae1904875111bbe5eaf
KYinXu <kyleyinxu@gmail.com>
2024-01-20 16:51:15 -0800

    fix navbar fading

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ window.onscroll = onScroll;
 var IS_SCROLLED = false;
 var FADING_IN = false;
 var FADING_OUT = false;
+var THRESHOLD = 15;
 function onScroll(){
     nav = document.getElementById("navigation-bar");
-    if (document.body.scrollTop >= 15 && IS_SCROLLED == false && FADING_IN == false){
+    if (document.body.scrollTop >= THRESHOLD && IS_SCROLLED == false && FADING_IN == false){
         FADING_OUT = false;
         FADING_IN = true;
         bgFadeIn(document.getElementById("navigation-bar"));
         IS_SCROLLED = true;
-    } else if (document.body.scrollTop < 15 && IS_SCROLLED == true && FADING_OUT == false){
+    } else if (document.body.scrollTop < THRESHOLD && IS_SCROLLED == true && FADING_OUT == false){
         FADING_IN = false;
         FADING_OUT = true;
         bgFadeOut(document.getElementById("navigation-bar"));
@@ -18,8 +19,17 @@ function onScroll(){
     }
 }
 
+//Fix fading condition checks. Make sure setting op doesn't disrupt
 function bgFadeIn(element) {
-    var op = 0.1;
+    console.log(element.style.backgroundColor);
+    var bgc = element.style.backgroundColor;
+    if (bgc == ""){
+        var op = 0.1;
+    } else {
+        var rgba = bgc.match(/(\d|[.])+/g);
+        console.log(rgba)
+        var op = Number(rgba[3]);
+    }
     var timer = setInterval(function(){
         if (op >= 0.7 || FADING_OUT == true){
             FADING_IN = false;
@@ -27,17 +37,26 @@ function bgFadeIn(element) {
         }
         element.style.backgroundColor = "rgba(0, 0, 0, " + op + ")";
         op += op * 0.1;
-    }, 7);
+        console.log("increasing: " + op);
+    }, 1);
 }
 
 function bgFadeOut (element) {
-    var op = 0.7;
+    console.log("fade out");
+    var bgc = element.style.backgroundColor;
+    if (bgc == ""){
+        var op = 0.7;
+    } else {
+        var rgba = bgc.match(/(\d|[.])+/g);
+        var op = Number(rgba[3]);
+    }
     var timer = setInterval(function(){
-        if (op <= 0.1 && FADING_IN == true){
+        if (op <= 0.1 || FADING_IN == true){
             FADING_OUT = false;
             clearInterval(timer);
         }
         element.style.backgroundColor = "rgba(0, 0, 0, " + op + ")";
         op -= op * 0.1;
-    }, 7);
+        console.log("decreasing: " + op);
+    }, 1);
 }
